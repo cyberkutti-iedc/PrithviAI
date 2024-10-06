a415b5820f2703388e3914fc57a08adab8a90dc9
AMALKRISHNA O U <amalkrishna34@gmail.com>
2024-10-06 22:53:06 +0530

    Add files via upload

---
+++ app/(default)/page.tsx
@@ -0,0 +1,24 @@
+export const metadata = {
+  title: "Home - Prithvi Ai",
+  description: "Page description",
+};
+
+import PageIllustration from "@/components/page-illustration";
+import Hero from "@/components/hero-home";
+import Workflows from "@/components/workflows";
+import Features from "@/components/features";
+
+import Cta from "@/components/cta";
+
+export default function Home() {
+  return (
+    <>
+      <PageIllustration />
+      <Hero />
+      <Workflows />
+      <Features />
+     
+      <Cta />
+    </>
+  );
+}
